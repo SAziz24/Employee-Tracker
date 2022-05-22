29e97fb15f08f983d688b07a7756aa2d20970843
SAziz24 <92948548+SAziz24@users.noreply.github.com>
2022-05-21 23:30:34 -0700

    uncommented connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
-// const mysql = require("mysql2");
+const mysql = require("mysql2");
 
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     // MySQL Username
-//     user: "Saziz24",
-//     // MySQL Password
-//     password: "wxyz1997!S",
-//     database: "employee_db"
-//   });
+const connection = mysql.createConnection({
+    host: "localhost",
+    // MySQL Username
+    user: "Saziz24",
+    // MySQL Password
+    password: "wxyz1997!S",
+    database: "employee_db"
+  });
   
-//   connection.connect(function (err) {
-//     if (err) throw err;
-//   });
+  connection.connect(function (err) {
+    if (err) throw err;
+  });
   
-//   module.exports = connection;
+  module.exports = connection;
